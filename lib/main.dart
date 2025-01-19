3fc30c6f6bbda25517a3ed78c72d0cb439afefdb
mohamedshalaby <shalabymohamed578@mail.com>
2025-01-19 16:24:37 +0200

    -save

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(); 
-  
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
