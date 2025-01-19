846e1857b7abd60df8b393bab827202c4d1d8109
mohamedshalaby <shalabymohamed578@mail.com>
2025-01-19 16:06:17 +0200

    -save

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load();
+  await dotenv.load(); 
+  
   runApp(const MyApp());
 }
 
